373b415188986741248463cb1acdd7d2cafe7499
Erik Engheim <erik.engheim@mac.com>
2014-09-19 11:36:06 +0200

    Scroll to active textfield when showing keyboard
    Initial solution. If keyboard won't come up reset your emulator. This will need some cleaning up as this was just first version.

---
+++ ScrollViewExamples/ScrollToActiveField/ScrollToActiveField/ViewController.swift
@@ -0,0 +1,65 @@
+//
+//  ViewController.swift
+//  ScrollToActiveField
+//
+//  Created by Erik Engheim on 18/09/14.
+//  Copyright (c) 2014 Translusion. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITextFieldDelegate {
+
+    @IBOutlet var scrollView: UIScrollView!
+    @IBOutlet var firstField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        registerKeyboardNotifications()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    deinit {
+        let center = NSNotificationCenter.defaultCenter()
+        center.removeObserver(self)
+    }
+    
+    func registerKeyboardNotifications() {
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWasShown:", name: UIKeyboardDidShowNotification, object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillBeHidden:", name: UIKeyboardWillHideNotification, object: nil)
+    }
+    
+    func keyboardWasShown(notification : NSNotification) {
+        let info = notification.userInfo
+        if let keyboardFrame: NSValue = info?[UIKeyboardFrameBeginUserInfoKey] as? NSValue {
+            let keyboardSize = keyboardFrame.CGRectValue().size
+            
+
+            scrollView.contentSize = view.frame.size
+            var frame = scrollView.frame
+            frame.size.height -= keyboardSize.height
+            scrollView.frame = frame
+            scrollView.scrollRectToVisible(firstField.frame, animated: true)
+        }
+    }
+    
+    
+    func keyboardWillBeHidden(notification : NSNotification) {
+        self.scrollView.frame = self.view.frame
+        scrollView.setContentOffset(CGPoint(x: 0, y: 0), animated: true)    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+
+    @IBAction func scrollBack(sender: AnyObject) {
+        self.scrollView.frame = self.view.frame
+        scrollView.setContentOffset(CGPoint(x: 0, y: 0), animated: true)
+    }
+}
+
