41c7c01c3f75df84fdd86006658cfe7e32a6c6b4
Erik Engheim <erik.engheim@mac.com>
2014-11-17 11:35:12 +0100

    Added singleview target for footerviewflipping

---
+++ TableViewExamples/FooterViewFlipping/FooterViewFlipping/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FooterViewFlipping
+//
+//  Created by Erik Engheim on 17/11/14.
+//
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
