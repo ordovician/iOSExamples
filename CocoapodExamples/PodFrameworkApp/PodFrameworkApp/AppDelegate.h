7232a80b772595309e58c4d948392924a1c84fbe
Erik Engheim <erik.engheim@mac.com>
2015-04-14 16:12:56 +0200

    For testing inclusion of Cocoapod dynamic frameworks in Objective-C

---
+++ CocoapodExamples/PodFrameworkApp/PodFrameworkApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PodFrameworkApp
+//
+//  Created by Erik Engheim on 14/04/15.
+//  Copyright (c) 2015 Pexip. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CocoapodExamples/PodFrameworkApp/PodFrameworkApp/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  PodFrameworkApp
+//
+//  Created by Erik Engheim on 14/04/15.
+//  Copyright (c) 2015 Pexip. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
